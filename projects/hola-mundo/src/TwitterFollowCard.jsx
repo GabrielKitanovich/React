759e26ee779d08b17a6bb0e44dda5827191b0f6d
Gabriel <gabriel.kitanovich@gmail.com>
2024-09-27 21:06:51 -0300

    Add TFC Reutilization

---
+++ projects/hola-mundo/src/TwitterFollowCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export function TwitterFollowCard({ avatarUrl, name, userName }) {
+    return (
+        <article className='tw-followCard'>
+            <header className='tw-followCard-header'>
+                <img className='tw-followCard-avatar' src={avatarUrl} alt={`La imagen de perfil de ${name}`} />
+                <div className='tw-followCard-info'>
+                    <strong>{name}</strong>
+                    <span className='tw-followCard-infoUserName'>@{userName}</span>
+                </div>
+            </header>
+
+            <aside>
+                <button className='tw-followCard-button'>
+                    <span>Seguir</span>
+                </button>
+            </aside>
+        </article>
+    );
+}
